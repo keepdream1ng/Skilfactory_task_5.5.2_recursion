7a3b2de421b7b0f596185b68ffa4106ae178155d
Anatolii Ivanov <gotnickname@gmail.com>
2023-03-17 19:44:14 +0530

    Add project files.

---
+++ Skilfactory_task_5.5.2_recursion/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Skilfactory_task_5._5._2_recursion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter phrase for echo/ Введите фразу для эха");
+            string phrase = Console.ReadLine();
+
+            Console.WriteLine("Enter echo deepness/ Введите глубину эха");
+            byte deepness = Byte.Parse(Console.ReadLine());
+
+            Echo(phrase, deepness);
+        }
+
+        static void Echo(string phrase, byte deepness)
+        {
+            if (deepness > 0) {
+                phrase = phrase.Remove(0, 2);
+                Console.WriteLine($"...{phrase}");
+                Echo(phrase, --deepness);
+            }
+
+        }
+    }
+}
